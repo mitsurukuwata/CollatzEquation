45340c6f69bad92fe2d9aca5cc5da57f5118db9d
Derin Can Kinet <68993575+mitsurukuwata@users.noreply.github.com>
2022-10-03 12:07:21 +0300

    Create Settings.cs

---
+++ Settings.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3x_1UI
+{
+    public class Settings
+    {
+        public Color? MainFormBackColor { get; set; }
+        public Color? Panel1BackColor { get; set; }
+        public Color? ChartBackColor { get; set; }
+        public Color? Panel2BackColor { get; set; }
+    }
+}
